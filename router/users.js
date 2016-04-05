ec1860f6fd8585e2979df1887660b541a3bf5506
kiki le singe <aalbertini95@gmail.com>
2016-04-05 20:59:27 +0200

    feat(router.users): fetch users from fake api

--- router/users.js
+++ router/users.js
@@ -1,3 +1,6 @@
+import 'isomorphic-fetch';
+import { find } from 'lodash/collection';
+
 export default (app, router) => {
   router
     .get(
@@ -5,15 +8,27 @@ export default (app, router) => {
       '/users/:id',
       (ctx, next) => {
         const { params } = ctx;
+        const { id } = params;
 
-        ctx.user = {
-          id: params.id,
-          name: 'Mickey',
-        };
-        return next();
+        return fetch('http://localhost:3000/users.json')
+          .then(response => response.json())
+          .then(users => find(users, { id: parseInt(id, 10) }))
+          .then(user => {
+            console.log(user);
+            ctx.user = user;
+
+            return next()
+          });
       },
       ctx => {
-        const { user: { id, name } } = ctx;
+        const { user } = ctx;
+
+        if (!user) {
+          ctx.status = 404;
+          return ctx.body = 'User not found';
+        }
+
+        const { id, name } = user;
 
         ctx.body = `id: ${id}, name: ${name}`;
       }
