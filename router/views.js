587bb48b99a1475cddf715406b3e54de73757981
kiki le singe <aalbertini95@gmail.com>
2016-04-05 17:04:10 +0200

    feat(router.views): add views routes

---
+++ router/views.js
@@ -0,0 +1,15 @@
+export default (app, router) => {
+  router
+    .get('/', (ctx) => {
+      ctx.body = 'Home';
+    })
+    .get('/hello', (ctx) => {
+      ctx.body = 'Hello World!';
+    })
+    .get('/about', (ctx) => {
+      ctx.body = 'About';
+    })
+    .get('/contact', (ctx) => {
+      ctx.body = 'Contact';
+    });
+};
