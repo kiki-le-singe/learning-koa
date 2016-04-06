379407038580152d8445c3427305b3238fb07577
kiki le singe <aalbertini95@gmail.com>
2016-04-06 12:59:09 +0200

    refactor(index): show debug message when server is started

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import Koa from 'koa';
 import serve from 'koa-static';
 import json from 'koa-json';
+import _debug from 'debug';
 
+import config from './config';
 import router from './router';
 
+const debug = _debug('app:server');
 const app = new Koa();
+const { SERVER_PORT } = config;
 
 router(app);
 
 app.use(json());
 app.use(serve('api'));
 
-app.listen(3000);
+app.listen(SERVER_PORT, () => {
+  debug(`API Server listening on http://localhost:${SERVER_PORT}`);
+});
